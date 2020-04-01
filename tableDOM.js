b3f7e6e810d70a78cd602bd578158861dd2e4c55
smurphy2230 <smurphy2230@gmail.com>
2020-04-01 15:56:46 -0400

    lab complete with DOM

--- tableDOM.js
+++ tableDOM.js
@@ -26,3 +26,47 @@ let r3c1Text = document.createTextNode("Pontiac");
 let r3c2Text = document.createTextNode("140mph");
 let r3c3Text = document.createTextNode("$20,000");
 
+// start with body element
+let docTree = document.body;
+
+docTree.appendChild(tableElement); // add table element
+docTree = docTree.lastChild; // move to table element
+docTree.appendChild(r1Element); // add row 1 element
+docTree = docTree.lastChild; // move to row 1 element
+docTree.appendChild(r1c1Element); // add cells to row 1
+docTree.appendChild(r1c2Element);
+docTree.appendChild(r1c3Element);
+docTree = docTree.firstChild; // move to first cell
+docTree.appendChild(r1c1Text); // add text to first cell
+docTree = docTree.nextSibling; // move to next cell
+docTree.appendChild(r1c2Text);
+docTree = docTree.nextSibling;
+docTree.appendChild(r1c3Text);
+docTree = docTree.parentNode;
+docTree = docTree.parentNode; // move back up into table
+
+docTree.appendChild(r2Element); // add second row with cells and data
+docTree = docTree.lastChild;
+docTree.appendChild(r2c1Element);
+docTree.appendChild(r2c2Element);
+docTree.appendChild(r2c3Element);
+docTree = docTree.firstChild;
+docTree.appendChild(r2c1Text);
+docTree = docTree.nextSibling;
+docTree.appendChild(r2c2Text);
+docTree = docTree.nextSibling;
+docTree.appendChild(r2c3Text);
+docTree = docTree.parentNode;
+docTree = docTree.parentNode; // move back up into table
+
+docTree.appendChild(r3Element); // add third row with cells and data
+docTree = docTree.lastChild;
+docTree.appendChild(r3c1Element);
+docTree.appendChild(r3c2Element);
+docTree.appendChild(r3c3Element);
+docTree = docTree.firstChild;
+docTree.appendChild(r3c1Text);
+docTree = docTree.nextSibling;
+docTree.appendChild(r3c2Text);
+docTree = docTree.nextSibling;
+docTree.appendChild(r3c3Text);
