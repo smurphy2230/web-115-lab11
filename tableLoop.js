0d97faca18224a6c8ece3c5b80d8e7ccd5452499
smurphy2230 <smurphy2230@gmail.com>
2020-04-03 14:32:51 -0400

    using loops to create table

--- tableLoop.js
+++ tableLoop.js
@@ -1,4 +1,33 @@
 // add html table using loops
+let tableData = [
+    ['Car', 'Top Speed', 'Price'],
+    ['Chevrolet', '120mph', '$10,000'],
+    ['Pontiac', '140mph', '$20,000']
+];
 
-let table = '';
-for ()
+let table = document.createElement('table');
+let tableBody = document.createElement('tbody');
+
+table.appendChild(tableBody);
+
+for(let i = 0; i < tableData.length; i++) {
+    let row = document.createElement('tr');
+    for(let j = 0; j < tableData[i].length; j++) {
+        let element = null;
+
+        if(i == 0) {
+            element = document.createElement('th');
+        } else {
+            element = document.createElement('td');
+        }
+        
+        element.innerText = tableData[i][j];
+
+        row.appendChild(element);
+    }
+
+    table.appendChild(row);
+
+}
+
+document.body.appendChild(table);
